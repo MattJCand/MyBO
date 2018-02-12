fd225505a543d24fcbb9e12eb83428ec8e469940
mathieu junes <matyjunes@gmail.com>
2018-02-12 15:50:23 +0100

    add home Back office

---
+++ home.php
@@ -0,0 +1,23 @@
+<?php include "templates/header.php"; ?>
+
+<div class="align homeLogo">
+  <img src="asset/img/logo_final.png" alt="logo Planete Manga">
+  <h1 class="align"> Accueil Back Office </h1>
+</div>
+
+<div id="linkBo">
+  <div class="containerLink">
+    <h1>test</h1>
+  </div>
+  <div class="containerLink">
+    <h1>test</h1>
+  </div>
+  <div class="containerLink">
+    <h1>test</h1>
+  </div>
+  <div class="containerLink">
+    <h1>test</h1>
+  </div>
+</div>
+
+<?php include "templates/footer.php"; ?>
