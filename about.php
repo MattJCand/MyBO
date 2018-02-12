6cd929e74b6308eaf2c0623eba6b270907bb057b
mathieu junes <matyjunes@gmail.com>
2018-02-12 14:24:49 +0100

    first commit

---
+++ about.php
@@ -0,0 +1,33 @@
+
+<?php include "templates/header.php"; ?>
+
+<h1>A propos</h1>
+
+<section class="sectionAbout">
+    <div class="aboutIntro">
+      <span>Title</span>
+      <p>Description</p>
+      <span>Title</span>
+      <p>Description</p>
+    </div>
+    <div class="containerAbout">
+      <div class="imageAbout">
+        <img src="#">
+      </div>
+      <div class="aboutDescription">
+        <p>Anthony Pretzman</p>
+        <p>Description</p>
+      </div>
+    </div>
+    <div class="containerAbout">
+      <div class="aboutDescription">
+        <p>Angry Panda</p>
+        <p>Description</p>
+      </div>
+      <div class="imageAbout">
+        <img src="#">
+      </div>
+    </div>
+</section>
+
+<?php include "templates/footer.php"; ?>
---
+++ index.php
@@ -0,0 +1,86 @@
+
+<?php include "templates/header.php"; ?>
+
+<section id="caroussel-banniere">
+    
+        <div class="fade">
+
+          <div class="bloc-banniere banniere-japonnais">
+            <div class="container">
+              <div class="bloc-titre-banniere">
+                <h1 class="majuscule">Japonnais</h1>
+              </div>
+
+              <div class="bloc-texte-banniere">
+                <p>
+                  Découverte des syllabaires (hiragana et katakana) et des premiers kanji de niveau élémentaire, apprendre à lire, à écrire et à comprendre des phrases relativement simples.
+                </p>
+              </div>
+            </div>
+          </div>
+
+
+          <div class="bloc-banniere banniere-manga">
+            <div class="container">
+              <div class="bloc-titre-banniere">
+                <h1 class="majuscule">manga</h1>
+              </div>
+
+              <div class="bloc-texte-banniere">
+                <p>
+                  Découverte des syllabaires (hiragana et katakana) et des premiers kanji de niveau élémentaire, apprendre à lire, à écrire et à comprendre des phrases relativement simples.
+                </p>
+              </div>
+            </div>
+          </div>
+
+
+          <div class="bloc-banniere banniere-dessin">
+            <div class="container">
+              <div class="bloc-titre-banniere">
+                <h1 class="majuscule">dessin</h1>
+              </div>
+
+              <div class="bloc-texte-banniere">
+                <p>
+                  Découverte des syllabaires (hiragana et katakana) et des premiers kanji de niveau élémentaire, apprendre à lire, à écrire et à comprendre des phrases relativement simples.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          
+            
+        </div>
+  
+</section>
+
+
+
+
+
+<section id="sectionCours">
+  <div class="container">
+    <div id="dessin"></div>
+    <div id="japonais"></div>
+    <div id="manga"></div>
+  </div>
+</section>
+
+<section id="carteMember">
+</section>
+
+<!-- <section id="social">
+  <h4>Actualités</h4>
+  <!-- <div id="actu">
+    <div id="fb-root"></div>
+    <div class="fb-page" data-href="https://www.facebook.com/planete.manga.muret/" data-tabs="timeline" data-width="500" data-height="400" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false"><blockquote cite="https://www.facebook.com/planete.manga.muret/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/planete.manga.muret/">Planète Manga</a></blockquote></div>
+  </div>
+
+</section> -->
+  <h4>Partenaires</h4>
+  <div id="partner"></div>
+</section>
+ -->
+<?php include "templates/footer.php"; ?>
+
