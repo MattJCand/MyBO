9d9bacb0815b618f54ad3966940b7492adbc6d87
mathieu junes <matyjunes@gmail.com>
2018-02-12 15:20:27 +0100

    add page Login

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 
 <?php include "templates/header.php"; ?>
 
-<h1 class="align">Back Office </h1>
+<div class="align" id="homeLogo">
+  <img src="asset/img/logo_final.png" alt="logo Planete Manga">
+  <h1 class="align">Back Office </h1>
+</div>
+
+<?php include "admin/connection.php"; ?>
 
 <?php include "templates/footer.php"; ?>
 
