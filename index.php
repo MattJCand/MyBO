78c15eda7614da8095ee149e555f2da33f1f4815
mathieu junes <matyjunes@gmail.com>
2018-02-12 14:44:56 +0100

    add second part

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 
 <?php include "templates/header.php"; ?>
 
+<h1 class="align">Back Office </h1>
 
 <?php include "templates/footer.php"; ?>
 
--- templates/header.php
+++ templates/header.php
@@ -2,12 +2,9 @@
 <html lang="fr">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title>Back Office</title>
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1">
-	<!-- Slick -->
-	<link rel="stylesheet" type="text/css" href="asset/slick/slick.css"/>
-  <link rel="stylesheet" type="text/css" href="asset/slick/slick-theme.css"/>
-  	<link rel="stylesheet" href="asset/css/style.css">
+  <link rel="stylesheet" href="asset/css/style.css">
 
 </head>
 <body>
