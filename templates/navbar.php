52287a4c177e3426538ef2b58f7334dec378fe8e
mathieu junes <matyjunes@gmail.com>
2018-03-03 18:53:43 +0100

    Back office V1.1 working

---
+++ templates/navbar.php
@@ -0,0 +1,32 @@
+<header>
+  <div class="container">
+    <div class="bloc-logo-nav">
+      <a href="pages/home/home.php">
+        <img src="http://localhost/~matt/MyBO/asset/img/logo_final.png" alt="logo Planete Manga">
+      </a>
+    </div>
+<!--    <div class="btn-menu-block">   -->
+      <input type="checkbox" id="btn-menu" checked>
+      <label for="btn-menu" >&#9776;</label>
+    <!-- </div> -->
+    <nav class="menu majuscule">
+      <ul class="text-center">
+        <li><a href="pages/home/home.php">Accueil</a></li>
+        <li class="submenu"><a href="pages/home.php">L'école</a>
+          <ul>
+            <li><a href="pages/galerie/galerie.php">Galerie</a></li>
+            <li><a href="pages/about/about.php">A propos</a></li>
+            <li><a href="pages/partner/partner.php">Nos partenaires</a></li>
+          </ul>
+        </li>
+        <li class="submenu">
+          <a href="pages/cours/cours.php">
+            Cours
+          </a>
+        <li><a href="pages/event/event">événements</a></li>
+        <li><a href="pages/contact/contact.php">Contact</a></li>
+        <li><a href="admin/logout.php">Déconnection</a></li>
+      </ul>
+    </nav>
+  </div>
+</header>
