5505dc2497b19bf533a71c38b7f72a894b722927
mathieu junes <matyjunes@gmail.com>
2018-03-10 15:45:13 +0100

    add event crud + fix bugs

--- pages/accueil/accueil.php
+++
@@ -1,6 +0,0 @@
-<?php include "../../templates/header_page.php"; ?>
-<?php include "../../admin/session.php"; ?>
-
-<h1 class="align">Index page Accueil</h1>
-
-<?php include "../../templates/footer.php"; ?>
--- pages/event/add.php
+++ pages/event/add.php
@@ -5,11 +5,12 @@
 
 <?php
     // $bdd = "INSERT INTO professeur(nom, prenom, profession, description, mobile, email VALUES (:nom, :prenom, :profession, :description , :mobile , :email )";
-$bdd="INSERT INTO evenement(titre, dateCrea, description, url, image_id) VALUES (:titre, :dateCrea, :description, :url, 1)";
+$bdd="INSERT INTO evenement(image, nom, description, lieu, url, horaire_id) VALUES (:image, :nom, :description, :lieu, :url, 1)";
     $req = $pdo->prepare($bdd);
-    $req->bindParam(':titre', $_POST['titre'], PDO::PARAM_STR);
-    $req->bindParam(':dateCrea', $_POST['dateCrea'], PDO::PARAM_STR);
+    $req->bindParam(':image', $_POST['image'], PDO::PARAM_STR);
+    $req->bindParam(':nom', $_POST['nom'], PDO::PARAM_STR);
     $req->bindParam(':description', $_POST['description'], PDO::PARAM_STR);
+    $req->bindParam(':lieu', $_POST['lieu'], PDO::PARAM_STR);
     $req->bindParam(':url', $_POST['url'], PDO::PARAM_STR);
     if ($req->execute()) {
        header('Location: event.php');
@@ -18,9 +19,10 @@ $bdd="INSERT INTO evenement(titre, dateCrea, description, url, image_id) VALUES 
 
 
 <form method="post">
-  <input name="titre" type="text"  placeholder="titre" value="<?php $titre;?>">
-  <input name="dateCrea" type="date" value="<?php $dateCrea;?>">
+  <input name="image" type="text"  placeholder="image" value="<?php $image;?>">
+  <input name="nom" type="text" value="<?php $nom;?>">
   <input name="description" type="text"  placeholder="Description" value="<?php $description;?>">
+  <input name="lieu" type="text" value="<?php $lieu;?>">
   <input name="url" type="text"  placeholder="Url" value="<?php $url;?>">
   <input type="submit" name="submit" value="submit">
 </form>
